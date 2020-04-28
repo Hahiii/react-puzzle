22f3dcc1b9c4a2196aa5ef644c4ee2c3c706af5c
Nazim Shala <shalanazim@gmail.com>
2020-04-28 21:45:48 +0200

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 
-import Puzzle from './components/game/puzzle';
+import Puzzle from './components/puzzle/puzzle';
 function App() {
   return (
     <div className="App">
--- src/components/box/box.js
+++ src/components/box/box.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './box.scss';
 
-function Box() {
+function Box({ value, onClick }) {
   return (
-    <div className="box">
-
-    </div>
+    <div className={`box ${!value && 'empty'}`} onClick={value ? ((event) => onClick(event.target)) : null}> {value}</div >
   )
 }
 
