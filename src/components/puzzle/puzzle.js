f93f0bd98af9d8e7e1c612eddf3984f0abaa1c16
Nazim Shala <shalanazim@gmail.com>
2020-04-28 13:11:04 +0200

    add puzzle component

---
+++ src/components/puzzle/puzzle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './puzzle.scss';
+
+import Box from '../box/box';
+
+function Puzzle() {
+  return (
+    <div className="puzzle-container">
+      <Box />
+    </div>
+  )
+}
+
+export default Puzzle;
