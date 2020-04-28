e01a7e18bf87d25435e441350b68e011a2a0a568
Nazim Shala <shalanazim@gmail.com>
2020-04-28 13:13:23 +0200

    update box scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 
+import Puzzle from './components/game/puzzle';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Puzzle</h1>
       </header>
+      <Puzzle />
     </div>
   );
 }
