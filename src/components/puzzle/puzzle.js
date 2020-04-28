4655f0456283a7c00d4a34a63a7d69fd9a92438e
Nazim Shala <shalanazim@gmail.com>
2020-04-28 21:45:18 +0200

    make it move

--- src/components/puzzle/puzzle.js
+++ src/components/puzzle/puzzle.js
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './puzzle.scss';
 
 import Box from '../box/box';
 
 function Puzzle() {
+  const [gamePuzzle, setGamePuzzle] = useState([])
+  const puzzleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ""];
+  const puzzleArrayMap = [];
+  let x = puzzleArray.length;
+  useEffect(() => {
+    while (x > 0) {
+      let rendomeIndex = Math.floor(Math.random() * puzzleArray.length);
+      let rendomeValue = puzzleArray[rendomeIndex];
+      puzzleArrayMap.indexOf(rendomeValue) === -1 ?
+        puzzleArrayMap.push(rendomeValue) : x++;
+      x--;
+    }
+    if (!x) {
+      setGamePuzzle([...puzzleArrayMap])
+    }
+  }, [])
+
+  const handleClick = (target) => {
+    let boxToMove = Number(target.innerText);
+    let tempArr = [...gamePuzzle];
+    let indexClick = tempArr.indexOf(boxToMove);
+    let indexEmpty = tempArr.indexOf("");
+
+    if (indexClick - indexEmpty === 4 || indexClick - indexEmpty === -4) {
+      tempArr.splice(indexEmpty, 1, boxToMove);
+      tempArr.splice(indexClick, 1, "");
+    }
+
+    if (indexClick - indexEmpty === 1 || indexClick - indexEmpty === -1) {
+      if (!isItFirstOrLast(indexClick, indexEmpty)) {
+        tempArr.splice(indexEmpty, 1, boxToMove);
+        tempArr.splice(indexClick, 1, "");
+      }
+    }
+    setGamePuzzle([...tempArr])
+  }
+
+  const isItFirstOrLast = (index, empty) => {
+    console.log(index);
+    if (index === 3 && empty === 4 || index === 7 && empty === 8 || index === 11 && empty === 12) {
+      return true;
+    }
+    if (index === 4 && empty === 3 || index === 8 && empty === 7 || index === 12 && empty === 11) {
+      return true;
+    }
+  }
+
+
   return (
     <div className="puzzle-container">
-      <Box />
+      {gamePuzzle.map((item, index) => {
+        return (
+          <Box
+            value={item}
+            key={index}
+            onClick={handleClick}
+          />
+        )
+      })
+      }
     </div>
   )
 }
