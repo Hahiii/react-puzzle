72ac3da431450a6848421f4419a3d93ccf090c71
Nazim Shala <shalanazim@gmail.com>
2020-04-29 17:47:19 +0200

    removed preview from app component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import './App.scss';
 import Puzzle from './components/puzzle/puzzle';
 import ImageSwitcher from './components/switcher/switcher';
 
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectPreview, selectTempArr, selectPuzzle } from './redux/puzzle/puzzle.selectors';
-import { setPreview } from './redux/puzzle/puzzle.action';
-
-
-
-function App({ preview, previewPuzzleArray, tempArr, puzzle }) {
-  const handleMouseEnter = () => {
-    previewPuzzleArray({ name: puzzle, array: [...preview] })
-  }
-
-  const handleMouseLeave = () => {
-    previewPuzzleArray({ name: puzzle, array: [...tempArr] })
-  }
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -26,22 +12,9 @@ function App({ preview, previewPuzzleArray, tempArr, puzzle }) {
       </header>
       <ImageSwitcher />
       <Puzzle />
-      {puzzle && <span
-        onMouseEnter={() => handleMouseEnter()}
-        onMouseLeave={() => handleMouseLeave()}>Preview</span>}
     </div>
   );
 }
 
-const mapDispatchToProps = dispatch => ({
-  previewPuzzleArray: (puzzleArray) => dispatch(setPreview(puzzleArray)),
-});
-
-const mapStateToProps = createStructuredSelector({
-  preview: selectPreview,
-  tempArr: selectTempArr,
-  puzzle: selectPuzzle
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
 
