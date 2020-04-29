e843bb6489548a8b71d441533113191dd36875e1
Nazim Shala <shalanazim@gmail.com>
2020-04-29 22:12:30 +0200

    update colors

--- src/App.js
+++ src/App.js
@@ -10,22 +10,32 @@ import { selectPuzzleArray, selectPreview, selectPreviewState, selectPuzzle } fr
 
 function App({ puzzleArray, preview, isPreview, puzzle }) {
   let isSolved = !isPreview && puzzleArray && preview.join("") === puzzleArray.join("");
+  const puzzleColor =
+  {
+    "pikachu": "#fdd827",
+    "numbers": "#d1cfcf",
+    "bob": "#c57f34",
+    "monsters": "#5d9820",
+    "bird": "#8acce6",
+    "": "#8acce6"
+  }
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Puzzle</h1>
+      <header className="App-header" style={{ background: `${puzzleColor[puzzle]}` }}>
+        <h1>Puzzle</h1>
       </header>
       <ImageSwitcher
         isSolved={isSolved}
       />
       <div className="solved-puzzle">
-        {isSolved && <h3>{`Well done..!! You solved ${puzzle.toUpperCase()}`}</h3>}
+        {isSolved && <h2>{`Well done..!! You solved ${puzzle.toUpperCase()}`}</h2>}
       </div>
       <Puzzle
         isSolved={isSolved}
       />
-    </div>
+    </div >
   );
 }
 
