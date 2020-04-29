b38c62c96e3d6bab3c8d31358f60e46a07681840
Nazim Shala <shalanazim@gmail.com>
2020-04-29 18:05:51 +0200

    update redux

--- src/redux/puzzle/puzzle.reducer.js
+++ src/redux/puzzle/puzzle.reducer.js
@@ -4,6 +4,8 @@ const INITIAL_STATE = {
   puzzle: "",
   pikachu: null,
   bob: null,
+  monsters: null,
+  bird: null,
   numbers: null,
   preview: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ""]
 }
