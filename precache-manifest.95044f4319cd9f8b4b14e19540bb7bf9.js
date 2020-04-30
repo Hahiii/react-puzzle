8940daaa819de68db0b807b3976fd7502850d426
Nazim Shala <shalanazim@gmail.com>
2020-04-30 16:20:39 +0200

    Updates

---
+++ precache-manifest.95044f4319cd9f8b4b14e19540bb7bf9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85eeec6ce40953470dbfe172a50ce1cd",
+    "url": "/react-puzzle/index.html"
+  },
+  {
+    "revision": "414571b4e6a996b08201",
+    "url": "/react-puzzle/static/css/main.9002f579.chunk.css"
+  },
+  {
+    "revision": "335062c00a8e15775567",
+    "url": "/react-puzzle/static/js/2.18131f6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-puzzle/static/js/2.18131f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414571b4e6a996b08201",
+    "url": "/react-puzzle/static/js/main.10811ef8.chunk.js"
+  },
+  {
+    "revision": "730c970db89318bd3594",
+    "url": "/react-puzzle/static/js/runtime-main.67b645bc.js"
+  },
+  {
+    "revision": "8f06d96817c73128cda7756fee2b877c",
+    "url": "/react-puzzle/static/media/bird.8f06d968.png"
+  },
+  {
+    "revision": "68efc56ffe024c1c9372dc4171d34ac1",
+    "url": "/react-puzzle/static/media/bob.68efc56f.png"
+  },
+  {
+    "revision": "a741b6c6c92e9a8d16d1594f38ea7d8f",
+    "url": "/react-puzzle/static/media/cricket.a741b6c6.png"
+  },
+  {
+    "revision": "941360fca10fbe6401b42058efa4fa34",
+    "url": "/react-puzzle/static/media/donald.941360fc.png"
+  },
+  {
+    "revision": "daaacfffd3d78560f476c9485c57f70b",
+    "url": "/react-puzzle/static/media/melman.daaacfff.png"
+  },
+  {
+    "revision": "5a8c5413dedea030a92f0adf1deb081a",
+    "url": "/react-puzzle/static/media/micky.5a8c5413.png"
+  },
+  {
+    "revision": "214fdfb655f6b56013cf5873b7549490",
+    "url": "/react-puzzle/static/media/minion.214fdfb6.png"
+  },
+  {
+    "revision": "8d57583feacfa9193896a629bd4ae18b",
+    "url": "/react-puzzle/static/media/minne.8d57583f.png"
+  },
+  {
+    "revision": "1a24c447721523d9f2f6f9ada39fd1bf",
+    "url": "/react-puzzle/static/media/monsters.1a24c447.png"
+  },
+  {
+    "revision": "096e3b1046f81ed7ea65d2ad8804f75a",
+    "url": "/react-puzzle/static/media/numbers.096e3b10.png"
+  },
+  {
+    "revision": "8bb3bb4a0bdc6b1af4882a9edd9c6806",
+    "url": "/react-puzzle/static/media/pikachu.8bb3bb4a.png"
+  }
+]);
