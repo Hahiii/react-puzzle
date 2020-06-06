9e444251809dd6e73567d746b004d0e8b9cc1534
Nazim Shala <shalanazim@gmail.com>
2020-06-06 16:41:30 +0200

    change colors

--- src/data/data.js
+++ src/data/data.js
@@ -1,68 +1,73 @@
-import pikachu from '../images/pikachu.png';
-import bob from '../images/bob.png';
-import numbers from '../images/numbers.png';
-import monsters from '../images/monsters.png';
-import bird from '../images/bird.png';
-import micky from '../images/micky.png';
-import minne from '../images/minne.png';
-import donald from '../images/donald.png';
-import melman from '../images/melman.png';
-import minion from '../images/minion.png';
-import cricket from '../images/cricket.png';
-
-
+import pikachu from "../images/pikachu.png";
+import bob from "../images/bob.png";
+import numbers from "../images/numbers.png";
+import monsters from "../images/monsters.png";
+import bird from "../images/bird.png";
+import micky from "../images/micky.png";
+import minne from "../images/minne.png";
+import donald from "../images/donald.png";
+import melman from "../images/melman.png";
+import minion from "../images/minion.png";
+import cricket from "../images/cricket.png";
 
 export const switchArray = [
   {
-    "url": numbers,
-    "name": "numbers"
-  }, {
-    "url": pikachu,
-    "name": "pikachu"
-  }, {
-    "url": cricket,
-    "name": "cricket"
-  }, {
-    "url": minne,
-    "name": "minne"
-  }, {
-    "url": bob,
-    "name": "bob"
-  }, {
-    "url": monsters,
-    "name": "monsters"
-  }, {
-    "url": donald,
-    "name": "donald"
-  }, {
-    "url": bird,
-    "name": "bird"
-  }, {
-    "url": micky,
-    "name": "micky"
-  }, {
-    "url": minion,
-    "name": "minion"
-  }, {
-    "url": melman,
-    "name": "melman"
-  }
+    url: numbers,
+    name: "numbers",
+  },
+  {
+    url: pikachu,
+    name: "pikachu",
+  },
+  {
+    url: cricket,
+    name: "cricket",
+  },
+  {
+    url: minne,
+    name: "minne",
+  },
+  {
+    url: bob,
+    name: "bob",
+  },
+  {
+    url: monsters,
+    name: "monsters",
+  },
+  {
+    url: donald,
+    name: "donald",
+  },
+  {
+    url: bird,
+    name: "bird",
+  },
+  {
+    url: micky,
+    name: "micky",
+  },
+  {
+    url: minion,
+    name: "minion",
+  },
+  {
+    url: melman,
+    name: "melman",
+  },
 ];
 
-
-
-export const puzzleColor =
-{
-  "": "#d1cfcf",
-  "pikachu": "#fdd827",
-  "numbers": "#d1cfcf",
-  "bob": "#c57f34",
-  "monsters": "#5d9820",
-  "bird": "#8acce6",
-  "donald": "#fcb869",
-  "micky": "#040302",
-  "minion": "#5983a5",
-  "minne": "#d84b8d",
-  "cricket": "#fda30b",
-  "melman": "#daca99",
-}
+export const puzzleColor = {
+  "": "#676767",
+  pikachu: "#e0991e",
+  numbers: "#676767",
+  bob: "#e0991e",
+  monsters: "#98cf41",
+  bird: "#6e96b8",
+  donald: "#e0991e",
+  micky: "#676767",
+  minion: "#6e96b8",
+  minne: "hotpink",
+  cricket: "#98cf41",
+  melman: "#e0991e",
+};
